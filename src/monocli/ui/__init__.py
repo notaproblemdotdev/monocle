002726a77137b02e23eb528f5b79a40987801b03
Piotr Grędowski <piotrgredowski@gmail.com>
2026-02-08 11:13:29 +0100

    chore(03-01): create UI package structure
    - Create src/monocli/ui/ directory with __init__.py
    - Create tests/ui/ directory with __init__.py
    - Export MergeRequestSection and WorkItemSection (to be implemented)

---
+++ src/monocli/ui/__init__.py
@@ -0,0 +1,9 @@
+"""UI package for monocli.
+
+This package provides Textual-based widgets for the dashboard interface,
+including section widgets for displaying merge requests and work items.
+"""
+
+from monocli.ui.sections import MergeRequestSection, WorkItemSection
+
+__all__ = ["MergeRequestSection", "WorkItemSection"]
